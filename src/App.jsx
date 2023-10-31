9e4d037eee1557cebb4deb121faed518d02aa96a
Vardagsbruket <134447488+Vardagsbruket@users.noreply.github.com>
2023-10-31 14:05:58 +0100

    initial commit exercise week 6

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,44 @@
 // Import required  hooks
+import { useEffect, useState } from "react";
 import { DogFact } from "./components/DogFact";
 
+
+
 export const App = () => {
+  
   // Hint: Initialize state for storing the dog fact
+  const [facts, setFacts] = useState([]);
+  const [loading, setLoading] = useState(false)
+
   // Hint: Define the API endpoint
+  const API = "https://dogapi.dog/api/v2/facts";
+
   // Hint: Create a function to fetch the dog fact
+  
+//fetchDog();
+
+
   // Hint: Use the useEffect hook to fetch the dog fact when the component mounts
+  useEffect(() => {
+    const fetchDog = () => {
+      if (loading) {
+      fetch(API)
+      .then(res => res.json())
+      .then(response => {setFacts(response.data); console.log(response.data)
+      setLoading(false)
+      }) .catch(Error=>console.error('Error fetching data:', Error))
+    }
+  }
+  fetchDog();
+  }, [loading])
+
+  const handleClick = () => {
+    setLoading(true);
+  }
 
   return (
     <div className="App">
-      <DogFact />
+      <DogFact facts={facts} handleClick={handleClick} loading={loading}/>
     </div>
   );
 };
--- src/components/DogFact.jsx
+++ src/components/DogFact.jsx
@@ -1,10 +1,25 @@
+
+
 // DogFact Component
-export const DogFact = () => {
+export const DogFact = ({facts, handleClick, loading}) => {
+
   // Hint: Extract the 'body' from 'attributes' of the 'fact' prop. We'll call this 'cleanFact'.
   // Cleaning our data means choosing and keeping a somewhat small fraction of the data, the most useful data.
 
   // Hint: Render the 'cleanFact' if available, otherwise show a loading message
-  return <div>dog fact here.</div>;
+  return <div>
+        {loading ? <div>Loading...</div> 
+        : 
+        <div>
+          <ul>
+            {facts.map((fact) => (
+              <li key={fact.id}><p>{fact.attributes.body}</p></li>
+              ))}
+          </ul>
+        </div>
+        }
+        <button onClick={handleClick}>Get dog info</button>
+    </div>;
 };
 
 // Hint: To use this component, import it in your main App component and pass the 'fact' prop to it.
